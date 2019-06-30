407957298f8450f5a4c9456edcda5964ba9ff5d0
unknown <ryan.cornelius2112@gmail.com>
2019-07-01 08:05:34 +1200

    Morning 1/7

---
+++ 08 Advanced Array Methods/every.js
@@ -0,0 +1,9 @@
+//Manually implementing every:
+function some(arr, callback) {
+    for (let i = 0; i < arr.length; i++){
+        if(!callback(arr[i], i, arr)){
+            return false;
+        }
+    }
+    return true;
+}
---
+++ 08 Advanced Array Methods/some.js
@@ -0,0 +1,9 @@
+//Manually implementing some:
+function some(arr, callback) {
+    for (let i = 0; i < arr.length; i++){
+        if(callback(arr[i], i, arr)){
+            return true;
+        }
+    }
+    return false;
+}
